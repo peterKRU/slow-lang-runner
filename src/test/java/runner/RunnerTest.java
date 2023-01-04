e1d0b785491feb729d730dccc65d616e6ec51472
Peter Krastanov <peter.krastanow@gmail.com>
2023-01-04 17:48:46 +0200

    build: set up maven project structure
    Add pom.xml. Add src/main/java/ and src/test/java/ directories. Add pom.xml file.
    Add JUnit5 dependencies (junit-jupiter-api and junit-jupiter-engine) to pom.xml. Set junit-jupiter version to 5.9.1 (latest as of 04.01.2023).
    Add sunfire plugin. Add maven-jar-plugin to pom.xml.
    Add Runner class and set it as project entry point in pom.xml.
    Add single test case to test if all imports are successfull.

---
+++ src/test/java/runner/RunnerTest.java
@@ -0,0 +1,14 @@
+package runner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RunnerTest {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
