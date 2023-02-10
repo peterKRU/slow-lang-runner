3f32e59166e671656d9678750c41b5ad28fa1d83
Peter Krastanov <peter.krastanow@gmail.com>
2023-02-10 13:36:34 +0200

    style: change default static message to slow-lang:

--- src/main/java/runner/Messages.java
+++ src/main/java/runner/Messages.java
@@ -2,7 +2,7 @@ package runner;
 
 public class Messages {
 	
-	public static final String DEFAULT_STATIC_MESSAGE = "SlowLang: ";
+	public static final String DEFAULT_STATIC_MESSAGE = "slow-lang: ";
 	public static final String COMPILE = "Compiling...";
 	public static final String RUN = "Running...";
 	public static final String TEST = "Running default test...";
