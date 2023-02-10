5214e25e71d16e4486810d2a3b5767b54899b4e3
Peter Krastanov <peter.krastanow@gmail.com>
2023-02-10 13:12:43 +0200

    feat: add Messages data holder class

---
+++ src/main/java/runner/Messages.java
@@ -0,0 +1,11 @@
+package runner;
+
+public class Messages {
+	
+	public static final String DEFAULT_STATIC_MESSAGE = "SlowLang: ";
+	public static final String COMPILE = "Compiling...";
+	public static final String RUN = "Running...";
+	public static final String TEST = "Running default test...";
+	
+	public static final String NOT_RECOGNIZED = "Command not recognized. ";
+}
