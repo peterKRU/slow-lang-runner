a1a2c6a47d0810a1b2867680d4b2cce3a435f808
Peter Krastanov <peter.krastanow@gmail.com>
2023-02-10 13:12:12 +0200

    feat: add Commands data holder class

---
+++ src/main/java/runner/Commands.java
@@ -0,0 +1,10 @@
+package runner;
+
+public class Commands {
+	
+	public static final String EXIT = "exit";
+	public static final String COMPILE = "compile";
+	public static final String RUN = "run";
+	public static final String COMPILE_AND_RUN = "compile&run";
+	public static final String TEST = "test";	
+}
